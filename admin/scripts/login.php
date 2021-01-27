c326ebcd89164b696f2928807fe9b4addfe5cf6f
Zhu Meng <z_meng@fanshaweonline.ca>
2021-01-27 07:03:38 -0500

    login page

---
+++ admin/scripts/login.php
@@ -0,0 +1,5 @@
+<!-- for any helper functions for login/log out -->
+<?php
+function login($username,$password){
+    return 'You are trying to login with Username:'.$username.'Password:'.$password;//remove it when done
+}
